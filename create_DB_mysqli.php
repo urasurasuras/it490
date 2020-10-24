59f8af12cb7f35d19504d604931dec409b8583d2
uras <uo7@njit.edu>
2020-10-24 13:35:20 -0400

    Now adds private profile bool and SR column to table

--- create_DB_mysqli.php
+++ create_DB_mysqli.php
@@ -35,7 +35,9 @@ $query = "CREATE TABLE IF NOT EXISTS ".$db_name.".".$table_name." (
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(30) NOT NULL,
     password VARCHAR(30) NOT NULL,
-    bnet     VARCHAR(20)
+    bnet     VARCHAR(20),
+	private BOOLEAN,
+	skillRating SMALLINT
     )";
     
 if ($conn->query($query) === TRUE) {
