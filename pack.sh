a1da481b2512a61b56a8b2042b79295bb1cea8f8
uras1 <uo7@njit.edu>
2020-11-15 23:43:09 -0500

    Feature: added packacging script, gitignored build folder

---
+++ pack.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+vb1=$(awk -F "=" '/VB1/ {print $2}' targets.ini)
+echo $vb1
+vb2=$(awk -F "=" '/VB2/ {print $2}' targets.ini)
+echo $vb2
+vb3=$(awk -F "=" '/VB3/ {print $2}' targets.ini)
+echo $vb3
+
+# echo testecho
+# scp -r $vb1:it490 ./
+# tar -zcvf it490.tgz it490/
+# rm -r it490
+#ssh user@remote "sudo scp -r user@local:/path/to/files /opt/bin"
+
+echo Pulling...
+echo
+
+mkdir package
+
+# Assume all scripts are in /it490
+
+# Pull libs and configs
+mkdir package/libs
+scp -r $vb3:~/it490/*.inc ./package/libs
+
+# Pull Client scripts
+mkdir package/client
+scp -r $vb3:~/it490/FrontEnd* ./package/client
+
+# Pull DB scripts
+mkdir package/database
+scp -r $vb3:~/it490/DB* ./package/database
+
+# Pull DMZ scripts
+mkdir package/datasource
+scp -r $vb3:~/it490/DMZ* ./package/datasource
+
+# Pull web page
+scp -r $vb1:/var/www/html ./package
+
+echo Packing...
+echo
+
+mkdir build
+tar -zcvf build/package.tgz package
+
+echo Cleaning...
+echo
+
+rm -r package
+
+echo DONE
+echo
